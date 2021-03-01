6cbf6b95cc168f9e9883dc905b2de13d0bb9d5d5
Sarah Lee <sarahselee223@gmail.com>
2021-02-28 20:54:58 -0800

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Sarah Lee on 2/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
